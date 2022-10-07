5d430823b7e5950d565d6eaccd96009d2f5768ef
Dbarry15 <Djenabou.barry@cuw.edu>
2022-10-07 12:09:21 -0500

    Initial commit

---
+++ src/net/codejava/module.java
@@ -0,0 +1,17 @@
+package net.codejava;
+
+public class module 
+{
+	public static void main(String[] args) 
+	{
+		int Djenabou = 13;
+		long Samadou  = 7700000000L;
+		int isatu = 5;
+		while (Djenabou <= Samadou)
+		{
+		    Djenabou = Djenabou * 3;
+		    isatu = isatu + 1;
+		}
+		System.out.println("it took " + isatu + " years");
+	}
+}
